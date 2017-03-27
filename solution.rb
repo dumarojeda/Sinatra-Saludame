cdd9e93652d820a164d956266136294d8ca580a6
Dumar Ojeda <dumarojeda@gmail.com>
2017-03-27 11:43:05 -0500

    Fix problem with var name

--- solution.rb
+++ solution.rb
@@ -1,12 +1,12 @@
 require 'sinatra'
 
 get '/' do
-  name = name
+  name = params[:nombre]
   name ? "<h1>Hola #{name}!</h1>" : '<h1>Hola desconocido!</h1>'
 end
 
 get '/makers/:nombre' do
-  name = name
+  name = params[:nombre]
   name.capitalize!
   name ? "<h1>Hola #{name}!</h1>" : '<h1>Hola desconocido!</h1>'
 end
